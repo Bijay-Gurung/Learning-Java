dce0adf0e9b4b3d49b920bcccb64a198d1c71064
Bijay-Gurung <stha4580@gmail.com>
2023-11-20 16:46:40 +0545

    Learning array

---
+++ Learning Java/arrays.java
@@ -0,0 +1,14 @@
+public class arrays{
+    public static void main(String[] args){
+        String[] sports = {"Football", "Volleyball", "Basketball", "Tennis"};
+        for(String i : sports){
+            System.out.println(i);
+        }
+        System.out.println(sports[0]);
+
+        int[] numbers = {1,2,3,4,5};
+        for(int j : numbers){
+            System.out.println(j);
+        }
+    }
+}
