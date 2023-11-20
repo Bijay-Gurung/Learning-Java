b44cb9be7c73ee95e82e2b17cf5ef77604eb8d6a
Bijay-Gurung <stha4580@gmail.com>
2023-11-20 21:39:54 +0545

    Completing Basics of Java\

--- Learning Java/arrays.java
+++ Learning Java/arrays.java
@@ -1,3 +1,4 @@
+/*
 public class arrays{
     public static void main(String[] args){
         String[] sports = {"Football", "Volleyball", "Basketball", "Tennis"};
@@ -6,9 +7,50 @@ public class arrays{
         }
         System.out.println(sports[0]);
 
+        sports[0] = "Mango"; // Change an Array Element
+        System.out.println(sports[0]);
+
         int[] numbers = {1,2,3,4,5};
         for(int j : numbers){
             System.out.println(j);
         }
+        // Array Length
+        System.out.println(numbers.length);
+    }
+}
+*/
+
+// Looping Through an Array
+public class arrays{
+    public static void main(String[] args){
+        // String[] Languages = {"English","Nepali","Hindi","Korean","Japanese"};
+        /*
+        for(String i : Languages){
+            System.out.println(i);
+        }
+        */
+
+       // For-Each Method
+       /*
+       for(int i=0; i < Languages.length;i++){
+        System.out.println(Languages[i]);
+       }
+       */
+
+      // Multidimensional Array
+      // It is used when we want to store data in tabular form. like, a table with rows and columns.
+      int [][] Num = {{1,2,3},{4,5,6}}; // Numbers is now an Array with 2 Arrays as it's elements
+      // System.out.println(Num[1][0]); // Acess Elements
+
+      // Change elements value
+      /* Num[1][0] = 8;
+      System.out.println(Num[1][0]); */
+
+      // Loop Through A Multidimensional Array
+      for(int i=0; i<Num.length; ++i){
+        for(int j=0; j<Num[i].length; ++j){
+            System.out.println(Num[i][j]);
+        }
+      }
     }
 }
