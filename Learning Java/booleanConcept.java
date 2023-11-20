5105d9978cd4597579337f9f6ea8f7e4fcdf89f2
Bijay-Gurung <stha4580@gmail.com>
2023-11-20 15:38:00 +0545

    learning if statement and switch case

--- Learning Java/booleanConcept.java
+++ Learning Java/booleanConcept.java
@@ -8,5 +8,24 @@ public class booleanConcept{
         int a = 50;
         int b = 10;
         System.out.println(a>b);
+        System.out.println(a == 50);
+        System.out.println(a == 10);
+
+        int myAge = 19;
+        int votingAge = 18;
+        System.out.println(myAge >= votingAge);
+
+        // if statement
+        if (myAge >= votingAge){
+            System.out.println("I am Eligible for Voting.");
+        }
+        else{
+            System.out.println("I am not Eligible for Voting.");
+        }
+
+        // Short Hand if statement
+        int x = 20;
+        String result = (x%2 == 0) ? "It is Even." : "It is Odd.";
+        System.out.println(result);
     }
 }
