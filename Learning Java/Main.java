7c65ace5707c289e33e50e143008e0b55d4a19d8
Bijay-Gurung <stha4580@gmail.com>
2023-11-20 11:00:47 +0545

    Basic Calculation

--- Learning Java/Main.java
+++ Learning Java/Main.java
@@ -3,5 +3,11 @@ public class Main{
     System.out.println("Hello World");
     System.out.println("I am Learning Java");
     System.out.println("It is awesome");
+    System.out.println("Println method prints in the new line");
+    System.out.print("Print method prints in the same line");
+    System.out.print("see");
+    System.out.println(1);
+    System.out.println(2);
+    System.out.println(3);
   }
 }
---
+++ Learning Java/basicCalculation.java
@@ -0,0 +1,8 @@
+public class basicCalculation{
+    public static void main(String[] args){
+        System.out.println(2+3);
+        System.out.println(3-2);
+        System.out.println(3*2);
+        System.out.println(10/2);
+    }
+}
